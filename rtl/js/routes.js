1ce5332c686f9c2feb22a1e462adfa6ddd261ac3
Alimosavi-Code <156713938+Alimosavi-Code@users.noreply.github.com>
2024-03-23 21:03:19 +0330

    New test design

---
+++ rtl/js/routes.js
@@ -0,0 +1,191 @@
+"use strict";
+var routes = [
+  {
+  path: '/',
+  Url: './index.html',
+  },
+  {
+    path: '/accordion/',
+    url: './elements/accordion.html',
+  },
+  {
+    path: '/badges/',
+    url: './elements/badges.html',
+  },
+  {
+    path: '/buttons/',
+    url: './elements/buttons.html',
+  },
+  {
+    path: '/cards/',
+    url: './elements/cards.html',
+  },
+  {
+    path: '/icons/',
+    url: './elements/icons.html',
+  },
+  {
+    path: '/list-view/',
+    url: './elements/list-view.html',
+  },
+  {
+    path: '/popover/',
+    url: './elements/popover.html',
+  },
+  {
+    path: '/popup/',
+    url: './elements/popup.html',
+  },
+  {
+    path: '/accordion/',
+    url: './elements/accordion.html',
+  },
+  {
+    path: '/slider/',
+    url: './elements/slider.html',
+  },
+  {
+    path: '/tooltip/',
+    url: './elements/tooltip.html',
+  },
+  {
+    path: '/about-us/',
+    url: './pages/about-us.html',
+  },
+  {
+    path: '/blog/',
+    url: './pages/blog.html',
+  },
+  {
+    path: '/blog-single/',
+    url: './pages/blog-single.html',
+  },
+  {
+    path: '/career/',
+    url: './pages/career.html',
+  },
+  {
+    path: '/client/',
+    url: './pages/client.html',
+  },
+  {
+    path: '/features/',
+    url: './pages/features.html',
+  },
+  {
+    path: '/gallery/',
+    url: './pages/gallery.html',
+  },
+  {
+    path: '/maintenance/',
+    url: './pages/maintenance.html',
+  },
+  {
+    path: '/page-not-found/',
+    url: './pages/page-not-found.html',
+  },
+  {
+    path: '/pricing-table/',
+    url: './pages/pricing-table.html',
+  },
+  {
+    path: '/services/',
+    url: './pages/services.html',
+  },
+  {
+    path: '/skill/',
+    url: './pages/skill.html',
+  },
+  {
+    path: '/walkthrough/',
+    url: './pages/walkthrough.html',
+  },
+  {
+    path: '/login/',
+    url: './apps/login.html',
+  },
+  {
+    path: '/register/',
+    url: './apps/register.html',
+  },
+  {
+    path: '/reset-password/',
+    url: './apps/reset-password.html',
+  },
+  {
+    path: '/contact-us/',
+    url: './pages/contact-us.html',
+  },
+  {
+    path: '/calendar/',
+    url: './apps/calendar.html',
+  },
+  {
+    path: '/profile/',
+    url: './apps/profile.html',
+  },
+  {
+    path: '/profile-edit/',
+    url: './apps/profile-edit.html',
+  },
+  {
+    path: '/embeds/',
+    url: './apps/embeds.html',
+  },
+  {
+    path: '/faq/',
+    url: './pages/faq.html',
+  },
+  {
+    path: '/maps/',
+    url: './apps/maps.html',
+  },
+  {
+    path: '/logout/',
+    url: './apps/logout.html',
+  },
+  {
+    path: '/shift-a-1402/',
+    url: './Shift/1402/shift a.html',
+  },
+  {
+    path: '/shift-b-1402/',
+    url: './Shift/1402/shift b.html',
+  },
+  {
+    path: '/shift-c-1402/',
+    url: './Shift/1402/shift c.html',
+  },
+  {
+    path: '/shift-d-1402/',
+    url: './Shift/1402/shift d.html',
+  },
+  {
+    path: '/lohe-1402/',
+    url: './Shift/1402/lohe.html',
+  },
+  {
+    path: '/shift-a/',
+    url: './Shift/1403/shift a.html',
+  },
+  {
+    path: '/shift-b/',
+    url: './Shift/1403/shift b.html',
+  },
+  {
+    path: '/shift-c/',
+    url: './Shift/1403/shift c.html',
+  },
+  {
+    path: '/shift-d/',
+    url: './Shift/1403/shift d.html',
+  },
+  {
+    path: '/lohe/',
+    url: './Shift/1403/lohe.html',
+  },
+  {
+    path: '/lohep/',
+    url: './Shift/1403/lohe.png',
+  },
+];
