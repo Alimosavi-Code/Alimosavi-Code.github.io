5db87a72f8df2d85d63d13ccc72bd2d884ccad79
Ali <mly20202020@gmail.com>
2025-05-23 18:14:25 +0330

    Revert Changes

--- rtl/js/routes.js
+++ rtl/js/routes.js
@@ -5,76 +5,8 @@ var routes = [
   Url: './index.html',
   },
   {
-    path: '/accordion/',
-    url: './elements/accordion.html',
-  },
-  {
-    path: '/badges/',
-    url: './elements/badges.html',
-  },
-  {
-    path: '/buttons/',
-    url: './elements/buttons.html',
-  },
-  {
-    path: '/cards/',
-    url: './elements/cards.html',
-  },
-  {
-    path: '/icons/',
-    url: './elements/icons.html',
-  },
-  {
-    path: '/list-view/',
-    url: './elements/list-view.html',
-  },
-  {
-    path: '/popover/',
-    url: './elements/popover.html',
-  },
-  {
-    path: '/popup/',
-    url: './elements/popup.html',
-  },
-  {
-    path: '/accordion/',
-    url: './elements/accordion.html',
-  },
-  {
-    path: '/slider/',
-    url: './elements/slider.html',
-  },
-  {
-    path: '/tooltip/',
-    url: './elements/tooltip.html',
-  },
-  {
     path: '/about-us/',
     url: './pages/about-us.html',
-  },
-  {
-    path: '/blog/',
-    url: './pages/blog.html',
-  },
-  {
-    path: '/blog-single/',
-    url: './pages/blog-single.html',
-  },
-  {
-    path: '/career/',
-    url: './pages/career.html',
-  },
-  {
-    path: '/client/',
-    url: './pages/client.html',
-  },
-  {
-    path: '/features/',
-    url: './pages/features.html',
-  },
-  {
-    path: '/gallery/',
-    url: './pages/gallery.html',
   },
   {
     path: '/maintenance/',
@@ -85,107 +17,652 @@ var routes = [
     url: './pages/page-not-found.html',
   },
   {
-    path: '/pricing-table/',
-    url: './pages/pricing-table.html',
-  },
-  {
-    path: '/services/',
-    url: './pages/services.html',
-  },
-  {
-    path: '/skill/',
-    url: './pages/skill.html',
-  },
-  {
     path: '/walkthrough/',
     url: './pages/walkthrough.html',
-  },
-  {
-    path: '/login/',
-    url: './apps/login.html',
-  },
-  {
-    path: '/register/',
-    url: './apps/register.html',
-  },
-  {
-    path: '/reset-password/',
-    url: './apps/reset-password.html',
   },
   {
     path: '/contact-us/',
     url: './pages/contact-us.html',
   },
   {
-    path: '/calendar/',
-    url: './apps/calendar.html',
+    path: '/shift-a-1q/',
+    url: './Shift/1q/shift a.html',
   },
   {
-    path: '/profile/',
-    url: './apps/profile.html',
+    path: '/shift-b-1q/',
+    url: './Shift/1q/shift b.html',
   },
   {
-    path: '/profile-edit/',
-    url: './apps/profile-edit.html',
+    path: '/shift-c-1q/',
+    url: './Shift/1q/shift c.html',
   },
   {
-    path: '/embeds/',
-    url: './apps/embeds.html',
+    path: '/shift-d-1q/',
+    url: './Shift/1q/shift d.html',
   },
   {
-    path: '/faq/',
-    url: './pages/faq.html',
+    path: '/lohe-1q/',
+    url: './Shift/1q/lohe.html',
   },
   {
-    path: '/maps/',
-    url: './apps/maps.html',
-  },
-  {
-    path: '/logout/',
-    url: './apps/logout.html',
-  },
-  {
-    path: '/shift-a-1402/',
-    url: './Shift/1402/shift a.html',
-  },
-  {
-    path: '/shift-b-1402/',
-    url: './Shift/1402/shift b.html',
-  },
-  {
-    path: '/shift-c-1402/',
-    url: './Shift/1402/shift c.html',
-  },
-  {
-    path: '/shift-d-1402/',
-    url: './Shift/1402/shift d.html',
-  },
-  {
-    path: '/lohe-1402/',
-    url: './Shift/1402/lohe.html',
+    path: '/lohe-1q-p/',
+    url: './Shift/1q/lohe.png',
   },
   {
     path: '/shift-a/',
-    url: './Shift/1403/shift a.html',
+    url: './Shift/2e/shift a.html',
   },
   {
     path: '/shift-b/',
-    url: './Shift/1403/shift b.html',
+    url: './Shift/2e/shift b.html',
   },
   {
     path: '/shift-c/',
-    url: './Shift/1403/shift c.html',
+    url: './Shift/2e/shift c.html',
   },
   {
     path: '/shift-d/',
-    url: './Shift/1403/shift d.html',
+    url: './Shift/2e/shift d.html',
+  },
+  {
+    path: '/shift-a-3b/',
+    url: './Shift/3b/shift a.html',
+  },
+  {
+    path: '/shift-b-3b/',
+    url: './Shift/3b/shift b.html',
+  },
+  {
+    path: '/shift-c-3b/',
+    url: './Shift/3b/shift c.html',
+  },
+  {
+    path: '/shift-d-3b/',
+    url: './Shift/3b/shift d.html',
   },
   {
     path: '/lohe/',
-    url: './Shift/1403/lohe.html',
+    url: './Shift/2e/lohe.html',
   },
   {
     path: '/lohep/',
-    url: './Shift/1403/lohe.png',
+    url: './Shift/2e/lohe.png',
+  },
+  {
+    path: '/1a/',
+    url: './Shift/2e/A/01.html',
+  },
+  {
+    path: '/2a/',
+    url: './Shift/2e/A/02.html',
+  },
+  {
+    path: '/3a/',
+    url: './Shift/2e/A/03.html',
+  },
+  {
+    path: '/4a/',
+    url: './Shift/2e/A/04.html',
+  },
+  {
+    path: '/5a/',
+    url: './Shift/2e/A/05.html',
+  },
+  {
+    path: '/6a/',
+    url: './Shift/2e/A/06.html',
+  },
+  {
+    path: '/7a/',
+    url: './Shift/2e/A/07.html',
+  },
+  {
+    path: '/8a/',
+    url: './Shift/2e/A/08.html',
+  },
+  {
+    path: '/9a/',
+    url: './Shift/2e/A/09.html',
+  },
+  {
+    path: '/10a/',
+    url: './Shift/2e/A/10.html',
+  },
+  {
+    path: '/11a/',
+    url: './Shift/2e/A/11.html',
+  },
+  {
+    path: '/12a/',
+    url: './Shift/2e/A/12.html',
+  },
+  {
+    path: '/1b/',
+    url: './Shift/2e/B/01.html',
+  },
+  {
+    path: '/2b/',
+    url: './Shift/2e/B/02.html',
+  },
+  {
+    path: '/3b/',
+    url: './Shift/2e/B/03.html',
+  },
+  {
+    path: '/4b/',
+    url: './Shift/2e/B/04.html',
+  },
+  {
+    path: '/5b/',
+    url: './Shift/2e/B/05.html',
+  },
+  {
+    path: '/6b/',
+    url: './Shift/2e/B/06.html',
+  },
+  {
+    path: '/7b/',
+    url: './Shift/2e/B/07.html',
+  },
+  {
+    path: '/8b/',
+    url: './Shift/2e/B/08.html',
+  },
+  {
+    path: '/9b/',
+    url: './Shift/2e/B/09.html',
+  },
+  {
+    path: '/10b/',
+    url: './Shift/2e/B/10.html',
+  },
+  {
+    path: '/11b/',
+    url: './Shift/2e/B/11.html',
+  },
+  {
+    path: '/12b/',
+    url: './Shift/2e/B/12.html',
+  },
+  {
+    path: '/1c/',
+    url: './Shift/2e/C/01.html',
+  },
+  {
+    path: '/2c/',
+    url: './Shift/2e/A/02.html',
+  },
+  {
+    path: '/3c/',
+    url: './Shift/2e/A/03.html',
+  },
+  {
+    path: '/4c/',
+    url: './Shift/2e/A/04.html',
+  },
+  {
+    path: '/5c/',
+    url: './Shift/2e/A/05.html',
+  },
+  {
+    path: '/6c/',
+    url: './Shift/2e/A/06.html',
+  },
+  {
+    path: '/7c/',
+    url: './Shift/2e/A/07.html',
+  },
+  {
+    path: '/8c/',
+    url: './Shift/2e/A/08.html',
+  },
+  {
+    path: '/9c/',
+    url: './Shift/2e/A/09.html',
+  },
+  {
+    path: '/10c/',
+    url: './Shift/2e/A/10.html',
+  },
+  {
+    path: '/11c/',
+    url: './Shift/2e/A/11.html',
+  },
+  {
+    path: '/12c/',
+    url: './Shift/2e/A/12.html',
+  },
+  {
+    path: '/1d/',
+    url: './Shift/2e/A/01.html',
+  },
+  {
+    path: '/2d/',
+    url: './Shift/2e/A/02.html',
+  },
+  {
+    path: '/3d/',
+    url: './Shift/2e/A/03.html',
+  },
+  {
+    path: '/4d/',
+    url: './Shift/2e/A/04.html',
+  },
+  {
+    path: '/5d/',
+    url: './Shift/2e/A/05.html',
+  },
+  {
+    path: '/6d/',
+    url: './Shift/2e/A/06.html',
+  },
+  {
+    path: '/7d/',
+    url: './Shift/2e/A/07.html',
+  },
+  {
+    path: '/8d/',
+    url: './Shift/2e/A/08.html',
+  },
+  {
+    path: '/9d/',
+    url: './Shift/2e/A/09.html',
+  },
+  {
+    path: '/10d/',
+    url: './Shift/2e/A/10.html',
+  },
+  {
+    path: '/11d/',
+    url: './Shift/2e/A/11.html',
+  },
+  {
+    path: '/12d/',
+    url: './Shift/2e/A/12.html',
+  },
+  {
+    path: '/1a-1q/',
+    url: './Shift/1q/A/01.html',
+  },
+  {
+    path: '/2a-1q/',
+    url: './Shift/1q/A/02.html',
+  },
+  {
+    path: '/3a-1q/',
+    url: './Shift/1q/A/03.html',
+  },
+  {
+    path: '/4a-1q/',
+    url: './Shift/1q/A/04.html',
+  },
+  {
+    path: '/5a-1q/',
+    url: './Shift/1q/A/05.html',
+  },
+  {
+    path: '/6a-1q/',
+    url: './Shift/1q/A/06.html',
+  },
+  {
+    path: '/7a-1q/',
+    url: './Shift/1q/A/07.html',
+  },
+  {
+    path: '/8a-1q/',
+    url: './Shift/1q/A/08.html',
+  },
+  {
+    path: '/9a-1q/',
+    url: './Shift/1q/A/09.html',
+  },
+  {
+    path: '/10a-1q/',
+    url: './Shift/1q/A/10.html',
+  },
+  {
+    path: '/11a-1q/',
+    url: './Shift/1q/A/11.html',
+  },
+  {
+    path: '/12a-1q/',
+    url: './Shift/1q/A/12.html',
+  },
+  {
+    path: '/1b-1q/',
+    url: './Shift/1q/B/01.html',
+  },
+  {
+    path: '/2b-1q/',
+    url: './Shift/1q/B/02.html',
+  },
+  {
+    path: '/3b-1q/',
+    url: './Shift/1q/B/03.html',
+  },
+  {
+    path: '/4b-1q/',
+    url: './Shift/1q/B/04.html',
+  },
+  {
+    path: '/5b-1q/',
+    url: './Shift/1q/B/05.html',
+  },
+  {
+    path: '/6b-1q/',
+    url: './Shift/1q/B/06.html',
+  },
+  {
+    path: '/7b-1q/',
+    url: './Shift/1q/B/07.html',
+  },
+  {
+    path: '/8b-1q/',
+    url: './Shift/1q/B/08.html',
+  },
+  {
+    path: '/9b-1q/',
+    url: './Shift/1q/B/09.html',
+  },
+  {
+    path: '/10b-1q/',
+    url: './Shift/1q/B/10.html',
+  },
+  {
+    path: '/11b-1q/',
+    url: './Shift/1q/B/11.html',
+  },
+  {
+    path: '/12b-1q/',
+    url: './Shift/1q/B/12.html',
+  },
+  {
+    path: '/1c-1q/',
+    url: './Shift/1q/C/01.html',
+  },
+  {
+    path: '/2c-1q/',
+    url: './Shift/1q/A/02.html',
+  },
+  {
+    path: '/3c-1q/',
+    url: './Shift/1q/A/03.html',
+  },
+  {
+    path: '/4c-1q/',
+    url: './Shift/1q/A/04.html',
+  },
+  {
+    path: '/5c-1q/',
+    url: './Shift/1q/A/05.html',
+  },
+  {
+    path: '/6c-1q/',
+    url: './Shift/1q/A/06.html',
+  },
+  {
+    path: '/7c-1q/',
+    url: './Shift/1q/A/07.html',
+  },
+  {
+    path: '/8c-1q/',
+    url: './Shift/1q/A/08.html',
+  },
+  {
+    path: '/9c-1q/',
+    url: './Shift/1q/A/09.html',
+  },
+  {
+    path: '/10c-1q/',
+    url: './Shift/1q/A/10.html',
+  },
+  {
+    path: '/11c-1q/',
+    url: './Shift/1q/A/11.html',
+  },
+  {
+    path: '/12c-1q/',
+    url: './Shift/1q/A/12.html',
+  },
+  {
+    path: '/1d-1q/',
+    url: './Shift/1q/A/01.html',
+  },
+  {
+    path: '/2d-1q/',
+    url: './Shift/1q/A/02.html',
+  },
+  {
+    path: '/3d-1q/',
+    url: './Shift/1q/A/03.html',
+  },
+  {
+    path: '/4d-1q/',
+    url: './Shift/1q/A/04.html',
+  },
+  {
+    path: '/5d-1q/',
+    url: './Shift/1q/A/05.html',
+  },
+  {
+    path: '/6d-1q/',
+    url: './Shift/1q/A/06.html',
+  },
+  {
+    path: '/7d-1q/',
+    url: './Shift/1q/A/07.html',
+  },
+  {
+    path: '/8d-1q/',
+    url: './Shift/1q/A/08.html',
+  },
+  {
+    path: '/9d-1q/',
+    url: './Shift/1q/A/09.html',
+  },
+  {
+    path: '/10d-1q/',
+    url: './Shift/1q/A/10.html',
+  },
+  {
+    path: '/11d-1q/',
+    url: './Shift/1q/A/11.html',
+  },
+  {
+    path: '/12d-1q/',
+    url: './Shift/1q/A/12.html',
+  },
+  
+  {
+    path: '/1a-3b/',
+    url: './Shift/3b/A/01.html',
+  },
+  {
+    path: '/2a-3b/',
+    url: './Shift/3b/A/02.html',
+  },
+  {
+    path: '/3a-3b/',
+    url: './Shift/3b/A/03.html',
+  },
+  {
+    path: '/4a-3b/',
+    url: './Shift/3b/A/04.html',
+  },
+  {
+    path: '/5a-3b/',
+    url: './Shift/3b/A/05.html',
+  },
+  {
+    path: '/6a-3b/',
+    url: './Shift/3b/A/06.html',
+  },
+  {
+    path: '/7a-3b/',
+    url: './Shift/3b/A/07.html',
+  },
+  {
+    path: '/8a-3b/',
+    url: './Shift/3b/A/08.html',
+  },
+  {
+    path: '/9a-3b/',
+    url: './Shift/3b/A/09.html',
+  },
+  {
+    path: '/10a-3b/',
+    url: './Shift/3b/A/10.html',
+  },
+  {
+    path: '/11a-3b/',
+    url: './Shift/3b/A/11.html',
+  },
+  {
+    path: '/12a-3b/',
+    url: './Shift/3b/A/12.html',
+  },
+  {
+    path: '/1b-3b/',
+    url: './Shift/3b/B/01.html',
+  },
+  {
+    path: '/2b-3b/',
+    url: './Shift/3b/B/02.html',
+  },
+  {
+    path: '/3b-3b/',
+    url: './Shift/3b/B/03.html',
+  },
+  {
+    path: '/4b-3b/',
+    url: './Shift/3b/B/04.html',
+  },
+  {
+    path: '/5b-3b/',
+    url: './Shift/3b/B/05.html',
+  },
+  {
+    path: '/6b-3b/',
+    url: './Shift/3b/B/06.html',
+  },
+  {
+    path: '/7b-3b/',
+    url: './Shift/3b/B/07.html',
+  },
+  {
+    path: '/8b-3b/',
+    url: './Shift/3b/B/08.html',
+  },
+  {
+    path: '/9b-3b/',
+    url: './Shift/3b/B/09.html',
+  },
+  {
+    path: '/10b-3b/',
+    url: './Shift/3b/B/10.html',
+  },
+  {
+    path: '/11b-3b/',
+    url: './Shift/3b/B/11.html',
+  },
+  {
+    path: '/12b-3b/',
+    url: './Shift/3b/B/12.html',
+  },
+  {
+    path: '/1c-3b/',
+    url: './Shift/3b/C/01.html',
+  },
+  {
+    path: '/2c-3b/',
+    url: './Shift/3b/A/02.html',
+  },
+  {
+    path: '/3c-3b/',
+    url: './Shift/3b/A/03.html',
+  },
+  {
+    path: '/4c-3b/',
+    url: './Shift/3b/A/04.html',
+  },
+  {
+    path: '/5c-3b/',
+    url: './Shift/3b/A/05.html',
+  },
+  {
+    path: '/6c-3b/',
+    url: './Shift/3b/A/06.html',
+  },
+  {
+    path: '/7c-3b/',
+    url: './Shift/3b/A/07.html',
+  },
+  {
+    path: '/8c-3b/',
+    url: './Shift/3b/A/08.html',
+  },
+  {
+    path: '/9c-3b/',
+    url: './Shift/3b/A/09.html',
+  },
+  {
+    path: '/10c-3b/',
+    url: './Shift/3b/A/10.html',
+  },
+  {
+    path: '/11c-3b/',
+    url: './Shift/3b/A/11.html',
+  },
+  {
+    path: '/12c-3b/',
+    url: './Shift/3b/A/12.html',
+  },
+  {
+    path: '/1d-3b/',
+    url: './Shift/3b/A/01.html',
+  },
+  {
+    path: '/2d-3b/',
+    url: './Shift/3b/A/02.html',
+  },
+  {
+    path: '/3d-3b/',
+    url: './Shift/3b/A/03.html',
+  },
+  {
+    path: '/4d-3b/',
+    url: './Shift/3b/A/04.html',
+  },
+  {
+    path: '/5d-3b/',
+    url: './Shift/3b/A/05.html',
+  },
+  {
+    path: '/6d-3b/',
+    url: './Shift/3b/A/06.html',
+  },
+  {
+    path: '/7d-3b/',
+    url: './Shift/3b/A/07.html',
+  },
+  {
+    path: '/8d-3b/',
+    url: './Shift/3b/A/08.html',
+  },
+  {
+    path: '/9d-3b/',
+    url: './Shift/3b/A/09.html',
+  },
+  {
+    path: '/10d-3b/',
+    url: './Shift/3b/A/10.html',
+  },
+  {
+    path: '/11d-3b/',
+    url: './Shift/3b/A/11.html',
+  },
+  {
+    path: '/12d-3b/',
+    url: './Shift/3b/A/12.html',
   },
 ];
